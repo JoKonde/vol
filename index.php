81b019e96144e2b5f99cac43736d0b60d980c8c7
Rachel Kamuanya <git config --global user.email rachkamuanya@gmail.com>
2024-08-11 17:25:50 +0100

    update

--- index.php
+++ index.php
@@ -127,39 +127,36 @@
   </div>
   <main class="page-wrapper">
     <!-- Sign In Modal-->
-    <div class="modal fade" id="signin-modal" tabindex="-1" aria-hidden="true">
+    <div class="modal fade" id="se-connecter" tabindex="-1" aria-hidden="true">
       <div class="modal-dialog modal-lg modal-dialog-centered p-2 my-0 mx-auto" style="max-width: 950px;">
         <div class="modal-content">
           <div class="modal-body px-0 py-2 py-sm-0">
             <button class="btn-close position-absolute top-0 end-0 mt-3 me-3" type="button" data-bs-dismiss="modal"></button>
             <div class="row mx-0 align-items-center">
               <div class="col-md-6 border-end-md p-4 p-sm-5">
-                <h2 class="h3 mb-4 mb-sm-5">Hey there!<br>Welcome back.</h2><img class="d-block mx-auto" src="img/signin-modal/signin.svg" width="344" alt="Illustartion">
-                <div class="mt-4 mt-sm-5">Don't have an account? <a href="#signup-modal" data-bs-toggle="modal" data-bs-dismiss="modal">Sign up here</a></div>
+                <h2 class="h3 mb-4 mb-sm-5">Salut!<br>Bienvenu.</h2><img class="d-block mx-auto" src="img/signin-modal/signin.svg" width="344" alt="Illustartion">
+                <div class="mt-4 mt-sm-5">Vous n'avez pas de compte? <a href="#signup-modal" data-bs-toggle="modal" data-bs-dismiss="modal">Créer en un ici</a></div>
               </div>
-              <div class="col-md-6 px-4 pt-2 pb-4 px-sm-5 pb-sm-5 pt-md-5"><a class="btn btn-outline-info rounded-pill w-100 mb-3" href="#"><i class="fi-google fs-lg me-1"></i>Sign in with Google</a><a class="btn btn-outline-info rounded-pill w-100 mb-3" href="#"><i class="fi-facebook fs-lg me-1"></i>Sign in with Facebook</a>
-                <div class="d-flex align-items-center py-3 mb-3">
-                  <hr class="w-100">
-                  <div class="px-3">Or</div>
-                  <hr class="w-100">
-                </div>
+              <div class="col-md-6 px-4 pt-2 pb-4 px-sm-5 pb-sm-5 pt-md-5">
+                
+                
                 <form class="needs-validation" novalidate>
                   <div class="mb-4">
-                    <label class="form-label mb-2" for="signin-email">Email address</label>
-                    <input class="form-control" type="email" id="signin-email" placeholder="Enter your email" required>
+                    <label class="form-label mb-2" for="signin-email">Email</label>
+                    <input class="form-control" type="email" id="signin-email" placeholder="Votre adresse email" required>
                   </div>
                   <div class="mb-4">
                     <div class="d-flex align-items-center justify-content-between mb-2">
-                      <label class="form-label mb-0" for="signin-password">Password</label><a class="fs-sm" href="#">Forgot password?</a>
+                      <label class="form-label mb-0" for="signin-password">Mot de Passe</label><a class="fs-sm" href="#">Mot de passe oublié?</a>
                     </div>
                     <div class="password-toggle">
-                      <input class="form-control" type="password" id="signin-password" placeholder="Enter password" required>
+                      <input class="form-control" type="password" id="signin-password" placeholder="Votre mot de passe" required>
                       <label class="password-toggle-btn" aria-label="Show/hide password">
                         <input class="password-toggle-check" type="checkbox"><span class="password-toggle-indicator"></span>
                       </label>
                     </div>
                   </div>
-                  <button class="btn btn-primary btn-lg rounded-pill w-100" type="submit">Sign in</button>
+                  <button class="btn btn-primary btn-lg rounded-pill w-100" type="submit">Se Connecter</button>
                 </form>
               </div>
             </div>
@@ -181,7 +178,7 @@
                   <li class="d-flex mb-2"><i class="fi-check-circle text-primary mt-1 me-2"></i><span>Easily manage your wishlist</span></li>
                   <li class="d-flex mb-0"><i class="fi-check-circle text-primary mt-1 me-2"></i><span>Leave reviews</span></li>
                 </ul><img class="d-block mx-auto" src="img/signin-modal/signup.svg" width="344" alt="Illustartion">
-                <div class="mt-sm-4 pt-md-3">Already have an account? <a href="#signin-modal" data-bs-toggle="modal" data-bs-dismiss="modal">Sign in</a></div>
+                <div class="mt-sm-4 pt-md-3">Already have an account? <a href="#se-connecter" data-bs-toggle="modal" data-bs-dismiss="modal">Sign in</a></div>
               </div>
               <div class="col-md-6 px-4 pt-2 pb-4 px-sm-5 pb-sm-5 pt-md-5"><a class="btn btn-outline-info rounded-pill w-100 mb-3" href="#"><i class="fi-google fs-lg me-1"></i>Sign in with Google</a><a class="btn btn-outline-info rounded-pill w-100 mb-3" href="#"><i class="fi-facebook fs-lg me-1"></i>Sign in with Facebook</a>
                 <div class="d-flex align-items-center py-3 mb-3">
@@ -231,7 +228,7 @@
     <!-- Navbar-->
     <header class="navbar navbar-expand-lg navbar-light bg-light fixed-top" data-scroll-header>
       <div class="container"><a class="navbar-brand me-3 me-xl-4" href="index.php"><img class="d-block" src="img/logo.png" width="90" alt="Finder"></a>
-        <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button><a class="btn btn-sm text-primary d-none d-lg-block order-lg-3" href="#signin-modal" data-bs-toggle="modal"><i class="fi-user me-2"></i>Connexion</a><a class="btn btn-primary btn-sm rounded-pill ms-2 order-lg-3" href="city-guide-add-business.html"><i class="fi-plus me-2"></i>Créer Compte</a>
+        <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button><a class="btn btn-sm text-primary d-none d-lg-block order-lg-3" href="#se-connecter" data-bs-toggle="modal"><i class="fi-user me-2"></i>Connexion</a><a class="btn btn-primary btn-sm rounded-pill ms-2 order-lg-3" href="#signup-modal" data-bs-toggle="modal"><i class="fi-plus me-2"></i>Créer Compte</a>
         <div class="collapse navbar-collapse order-lg-2" id="navbarNav">
           <ul class="navbar-nav navbar-nav-scroll" style="max-height: 35rem;">
             <!-- Demos switcher-->
